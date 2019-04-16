584f4ed66abf9e29fd128f4d98aa66d39c4e31b2
Jeferson Bujaico <jeferson.bujaico@tecsup.edu.pe>
2019-04-16 17:06:25 -0500

    Initial Commit

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by Jeferson Bujaico on 4/16/19.
+//  Copyright © 2019 JeffRay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
